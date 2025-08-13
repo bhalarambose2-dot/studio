a0a818c7659140de254386a58fa4604ffa11a55d
Bhala Ram Bose <bhalarambose2@gmail.com>
2025-08-13 06:58:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/newSeasonDestinations.tsx
+++ src/app/newSeasonDestinations.tsx
@@ -2,7 +2,7 @@
 'use client';
 export const newSeasonDestinations = [
   { name: 'Shimla, Himachal Pradesh', image: 'https://images.unsplash.com/photo-1489933504786-389c51eb1b7f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxTaW1sYSUyMHxlbnwwfHx8fDE3NTUwNjgyMjZ8MA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'shimla snow' },
-  { name: 'Manali, Himachal Pradesh', image: 'https://images.unsplash.com/photo-1621253974479-de0a2a53d3c7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxtYW5hbGl8ZW58MHx8fHwxNzU1MDYyNjM4fDA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'manali mountains' },
+  { name: 'Manali, Himachal Pradesh', image: 'https://images.unsplash.com/photo-1579689189009-874f5cac2db5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw4fHxNYW5hbGklMjB8ZW58MHx8fHwxNzU1MDY4MzAyfDA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'manali mountains' },
   { name: 'Auli, Uttarakhand', image: 'https://images.unsplash.com/photo-1617578025283-d5a8c9e057a6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxhdWxpfGVufDB8fHx8MTc1NTExODc0MXww&ixlib=rb-4.1.0&q=80&w=1080', hint: 'auli skiing' },
   { name: 'Gulmarg, Jammu & Kashmir', image: 'https://images.unsplash.com/photo-1618485246430-6a9a3b63a0d1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxndWxtYXJnfGVufDB8fHx8MTc1NTA2MDQ5NXww&ixlib=rb-4.1.0&q=80&w=1080', hint: 'gulmarg gondola' },
 ];
