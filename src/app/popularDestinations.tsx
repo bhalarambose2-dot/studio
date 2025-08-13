de233c52061faf80f4eab30b6309ab3cfaced547
Bhala Ram Bose <bhalarambose2@gmail.com>
2025-08-13 05:04:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/popularDestinations.tsx
+++ src/app/popularDestinations.tsx
@@ -8,6 +8,6 @@ export const popularDestinations = [
   { name: 'Kerala, India', image: 'https://images.unsplash.com/photo-1602216056096-3b40cc0c9944?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxrZXJhbGElMjBiYWNrd2F0ZXJzfGVufDB8fHx8MTc1NTExODc0MXww&ixlib=rb-4.1.0&q=80&w=1080', hint: 'kerala backwaters' },
   { name: 'Jaisalmer, Rajasthan', image: 'https://images.unsplash.com/photo-1713349881676-594b95a5742b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNHx8SmFpc2FsbWVyJTIwfGVufDB8fHx8MTc1NTA2MDQ5NXww&ixlib=rb-4.1.0&q=80&w=1080', hint: 'jaisalmer fort' },
   { name: 'Jodhpur, Rajasthan', image: 'https://images.unsplash.com/photo-1721973733816-1791a072295a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxOXx8Sm9kaHB1ciUyMHxlbnwwfHx8fDE3NTUwNjA3ODd8MA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'jodhpur fort' },
-  { name: 'Rishikesh, Uttarakhand', image: 'https://placehold.co/1080x1080.png', hint: 'rishikesh bridge' },
+  { name: 'Rishikesh, Uttarakhand', image: 'https://images.unsplash.com/photo-1650341259809-9314b0de9268?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxSaXNoaWtlc2glMjB8ZW58MHx8fHwxNzU1MDYxMzMzfDA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'rishikesh bridge' },
   { name: 'Nainital, Uttarakhand', image: 'https://placehold.co/1080x1080.png', hint: 'nainital lake' },
 ];
