b139d2fed14074c073da2086050e2955f1f30f66
Bhala Ram Bose <bhalarambose2@gmail.com>
2025-08-13 05:01:21 +0000

    Yes

--- src/app/popularDestinations.tsx
+++ src/app/popularDestinations.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 export const popularDestinations = [
   { name: 'Jaipur, Rajasthan', image: 'https://images.unsplash.com/photo-1477587458883-47145ed94245?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw0fHxKYWlwdXIlMjB8ZW58MHx8fHwxNzU1MDU2MTg4fDA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'jaipur palace' },
@@ -7,6 +8,6 @@ export const popularDestinations = [
   { name: 'Kerala, India', image: 'https://images.unsplash.com/photo-1602216056096-3b40cc0c9944?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxrZXJhbGElMjBiYWNrd2F0ZXJzfGVufDB8fHx8MTc1NTExODc0MXww&ixlib=rb-4.1.0&q=80&w=1080', hint: 'kerala backwaters' },
   { name: 'Jaisalmer, Rajasthan', image: 'https://images.unsplash.com/photo-1713349881676-594b95a5742b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxNHx8SmFpc2FsbWVyJTIwfGVufDB8fHx8MTc1NTA2MDQ5NXww&ixlib=rb-4.1.0&q=80&w=1080', hint: 'jaisalmer fort' },
   { name: 'Jodhpur, Rajasthan', image: 'https://images.unsplash.com/photo-1721973733816-1791a072295a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxOXx8Sm9kaHB1ciUyMHxlbnwwfHx8fDE3NTUwNjA3ODd8MA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'jodhpur fort' },
+  { name: 'Rishikesh, Uttarakhand', image: 'https://placehold.co/1080x1080.png', hint: 'rishikesh bridge' },
+  { name: 'Nainital, Uttarakhand', image: 'https://placehold.co/1080x1080.png', hint: 'nainital lake' },
 ];
-
-    
