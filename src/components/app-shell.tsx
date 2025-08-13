beb1e112a608d25b4694451819a07ad4dde3accd
Bhala Ram Bose <bhalarambose2@gmail.com>
2025-08-13 06:24:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button>
    
    with the

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -97,9 +97,6 @@ export function AppShell({ children }: { children: React.ReactNode }) {
             <div className="ml-auto flex-1 sm:flex-initial">
               {/* Search bar could go here */}
             </div>
-             <Link href="/auth">
-                <Button>Login / Sign Up</Button>
-            </Link>
         </div>
       </header>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
