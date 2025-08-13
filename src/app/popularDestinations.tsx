2b7179b70cbef6d247e9c8baf6e14b3ff6607ac5
Bhala Ram Bose <bhalarambose2@gmail.com>
2025-08-13 05:22:51 +0000

    Add madya Pradesh all trevaling place

--- src/app/popularDestinations.tsx
+++ src/app/popularDestinations.tsx
@@ -13,4 +13,6 @@ export const popularDestinations = [
   { name: 'Mumbai, Maharashtra', image: 'https://images.unsplash.com/photo-1660145416818-b9a2b1a1f193?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxNdW1iYWklMjB8ZW58MHx8fHwxNzU1MDYxNzUzfDA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'mumbai skyline' },
   { name: 'Agra, Uttar Pradesh', image: 'https://images.unsplash.com/photo-1524492412937-b28074a5d7da?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHx0YWolMjBtYWhhbHxlbnwwfHx8fDE3NTUwNjE4MjB8MA&ixlib=rb-4.1.0&q=80&w=1080', hint: 'taj mahal' },
   { name: 'Vrindavan, Uttar Pradesh', image: 'https://images.unsplash.com/photo-1707938233687-47e61e5ad7c4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxQcmVtJTIwbWFuZGlyJTIwfGVufDB8fHx8MTc1NTA2MjI3NXww&ixlib=rb-4.1.0&q=80&w=1080', hint: 'vrindavan temple' },
+  { name: 'Khajuraho, Madhya Pradesh', image: 'https://placehold.co/1080x1080.png', hint: 'khajuraho temple' },
+  { name: 'Ujjain, Madhya Pradesh', image: 'https://placehold.co/1080x1080.png', hint: 'ujjain temple' },
 ];
