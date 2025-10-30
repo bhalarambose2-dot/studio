9852bdffcab2d838947d4f1425a11bf78abb9cad
Bhala Ram Bose <bhalarambose2@gmail.com>
2025-10-30 12:46:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "br-trip",
+  "appId": "1:477570603875:web:38836186229ec07496d156",
+  "apiKey": "AIzaSyCacBmyQBXhGUZHHGO9fap-DieYF3casQg",
+  "authDomain": "br-trip.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "477570603875"
+};
