0ae8565654649b95191186b1c33d0432109d75bf
bhalarambose2-dot <bhalarambose2@gmail.com>
2026-03-28 10:22:59 +0530

    Update Firebase configuration with new project details

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -4,12 +4,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MSG_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC2MPagJSfgitEdpr_jKJ073MKLd7jjSNc",
+  authDomain: "br-trip.firebaseapp.com",
+  databaseURL: "https://br-trip-default-rtdb.firebaseio.com",
+  projectId: "br-trip",
+  storageBucket: "br-trip.firebasestorage.app",
+  messagingSenderId: "477570603875",
+  appId: "1:477570603875:web:f371ec9a017f97f696d156",
+  measurementId: "G-PJSNT1Q1LL"
 };
 
 const app = initializeApp(firebaseConfig);
