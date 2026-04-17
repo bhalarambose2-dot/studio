77db755985fb8471f742bd416fe7a15d9d844263
bhalarambose2-dot <bhalarambose2@gmail.com>
2026-04-17 06:52:38 +0530

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,22 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-storage-compat.js"></script>
 
+<script>
 const firebaseConfig = {
   apiKey: "AIzaSyC2MPagJSfgitEdpr_jKJ073MKLd7jjSNc",
   authDomain: "br-trip.firebaseapp.com",
-  databaseURL: "https://br-trip-default-rtdb.firebaseio.com",
   projectId: "br-trip",
   storageBucket: "br-trip.firebasestorage.app",
   messagingSenderId: "477570603875",
-  appId: "1:477570603875:web:f371ec9a017f97f696d156",
-  measurementId: "G-PJSNT1Q1LL"
+  appId: "1:477570603875:web:f371ec9a017f97f696d156"
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const ADMIN_EMAIL = "bhalarambose2@gmail.com";
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+</script>
