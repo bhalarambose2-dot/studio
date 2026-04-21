523d617c787a7b9c87bf73846e6b3669ed564392
bhalarambose2-dot <bhalarambose2@gmail.com>
2026-04-21 22:53:35 +0530

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,21 +1,16 @@
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-storage-compat.js"></script>
-
-<script>
+// Firebase Setup
 const firebaseConfig = {
   apiKey: "AIzaSyC2MPagJSfgitEdpr_jKJ073MKLd7jjSNc",
   authDomain: "br-trip.firebaseapp.com",
+  databaseURL: "https://br-trip-default-rtdb.firebaseio.com",
   projectId: "br-trip",
   storageBucket: "br-trip.firebasestorage.app",
   messagingSenderId: "477570603875",
-  appId: "1:477570603875:web:f371ec9a017f97f696d156"
+  appId: "1:477570603875:web:f371ec9a017f97f696d156",
+  measurementId: "G-PJSNT1Q1LL"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
 const db = firebase.firestore();
-const storage = firebase.storage();
-</script>
+const auth = firebase.auth();
